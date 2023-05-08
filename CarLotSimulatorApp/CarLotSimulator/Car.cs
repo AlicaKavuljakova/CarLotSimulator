20c73cfe417a49dbad06dac7607b0cf8fb7701ff
alica kavuljakova <drobeek@gmail.com>
2023-05-08 15:14:36 -0700

    homework

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{
+    public class Car
+    {
+        //Create a seperate class file called Car
+       public Car() //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
+        {
+
+        }
+        public Car(int year,string make,string model,string engineNoise, string honkNoise, bool isDrivable)
+        {
+            Year = year;
+            Make = make;
+            Model = model;
+            EngineNoise = engineNoise;
+            HonkNoise = honkNoise;
+            IsDriveable = isDrivable;
+        }
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model{ get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDriveable { get; set; }
+        //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
+        public void MakeEngineNoise()
+        {
+    
+            Console.WriteLine(  EngineNoise);
+        }
+        public void MakeHonkNoise(string honkNoise)
+        {
+       
+            Console.WriteLine(  honkNoise);
+        }
+        //The methods should take one string parameter: the respective noise property
+
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -8,15 +8,47 @@ namespace CarLotSimulator
         {
             //TODO
 
-            //Create a seperate class file called Car
-            //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
-            //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
-            //The methods should take one string parameter: the respective noise property
 
-
+            var carlot= new Carlot();
+           
             //Now that the Car class is created we can instanciate 3 new cars
+           
+            var car2 = new Car();
+            
             //Set the properties for each of the cars
+           car2.Make = "Chevy";
+            car2.Model = "Malibu";
+            car2.Year = 2010;
+            car2.EngineNoise = "weeeeum";
+            car2.HonkNoise = "beep beep";
+            car2.IsDriveable = true;
+            carlot.Cars.Add(car2);
+
+            var davesCar = new Car()
+            {
+                Year = 2013,
+                Model = "ForTwo",
+                Make = "Smart",
+                EngineNoise="Wrrom caput",
+                HonkNoise="beep-beep",
+                IsDriveable=!true
+            };
+            carlot.Cars.Add(davesCar);
+
+            var aliCar = new Car(2013,"Toyota","Corolla","purrrr","beep",true);
+            carlot.Cars.Add(aliCar);
+            carlot.Cars.Add(new Car(2011,"Nissan","Sentra","wrrrrrom","pitatata",false));
+       
             //Call each of the methods for each car
+            Console.WriteLine("Engine noises:");
+            car2.MakeEngineNoise();
+            aliCar.MakeEngineNoise();
+            davesCar.MakeEngineNoise();
+            Console.WriteLine("Honk noises:");
+            car2.MakeHonkNoise(car2.HonkNoise);
+            aliCar.MakeHonkNoise(aliCar.HonkNoise);
+            davesCar.MakeHonkNoise(davesCar.HonkNoise);
+
 
             //*************BONUS*************//
 
@@ -28,6 +60,11 @@ namespace CarLotSimulator
             //It should have at least one property: a List of cars
             //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
             //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+            Console.WriteLine("These cars are in carlot:");
+            foreach (var item in carlot.Cars)
+            {
+                Console.WriteLine($"Year:{item.Year}  Make: {item.Make}  Model: {item.Model}");
+            }
         }
     }
 }
